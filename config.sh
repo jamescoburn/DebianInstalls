9a77b76393044f89b6edb08ccb825f739a051dec
James Coburn <james.coburn@live.com>
2022-11-29 14:20:16 -0700

    Various updates

--- config.sh
+++ config.sh
@@ -5,32 +5,34 @@
 install cramfs /bin/false
 blacklist cramfs
 EOT
-modprobe -r cramfs
+/sbin/modprobe -r cramfs
     
 # Disable squashfs
 /bin/cat << EOT >/etc/modprobe.d/squashfs.conf
 install squashfs /bin/false
 blacklist squashfs
 EOT
-modprobe -r squashfs
+/sbin/modprobe -r squashfs
     
 # Disable udf
 /bin/cat << EOT >/etc/modprobe.d/udf.conf
 install udf /bin/false
 blacklist udf
 EOT
-modprobe -r udf
+/sbin/modprobe -r udf
 
 # Disable usb-storage
 /bin/cat << EOT >/etc/modprobe.d/usb-storage.conf
 install usb-storage /bin/false
 blacklist usb-storage
 EOT
-modprobe -r usb-storage
+/sbin/modprobe -r usb-storage
 
 # File system configuration
 /bin/sed -ri 's/^\s*([^#]+\s+\/tmp\s+)(\S+\s+)(\S+)?(\s+[0-9]\s+[0-9].*)$/\1\2\3,noexec\4/' /etc/fstab
+/bin/mount -o remount /tmp
 /bin/echo -e "tmpfs\t\t/dev/shm\ttmpfs\tnodev,nosuid,noexec\t0\t0" >> /etc/fstab
+/bin/mount -o remount /dev/shm
 
 # Secure boot settings
 /bin/cat << EOT >/etc/grub.d/42_custom
@@ -39,7 +41,7 @@ set superusers="root"
 password pbkdf2 root grub.pbkdf2.sha512.10000.080D3601C9F8BE1447DDAAA555D68D3952EF37B85AD60643F85FAD9FB22B0AB44D62F474BD6CADF8BD9C1E5CC3A2BFD719ED69FF91EB376E2173CA76F3DD79C9.7D241BF1338A9129600D559CE8DF57B5618019EEBCEE7F39A030D116C79F3F941A496CD411975B9DFCE3C82CE93AD48B6C1DA76093806BCDF7B4D5220F0D8582
 EOF
 EOT
-chmod u-wx,go-rwx /boot/grub/grub.cfg
+/bin/chmod u-wx,go-rwx /boot/grub/grub.cfg
 
 # Additional Process Hardening
 /bin/printf " kernel.randomize_va_space = 2 " >> /etc/sysctl.d/60-kernel_sysctl.conf
