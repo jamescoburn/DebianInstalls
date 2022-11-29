f04a243fe220c12fbb35b2d34af8eab1415c2e6e
James Coburn <james.coburn@live.com>
2022-11-29 14:04:11 -0700

    Updated disable sections

--- config.sh
+++ config.sh
@@ -5,24 +5,28 @@
 install cramfs /bin/false
 blacklist cramfs
 EOT
+modprobe -r cramfs
     
 # Disable squashfs
 /bin/cat << EOT >/etc/modprobe.d/squashfs.conf
 install squashfs /bin/false
 blacklist squashfs
 EOT
+modprobe -r squashfs
     
 # Disable udf
 /bin/cat << EOT >/etc/modprobe.d/udf.conf
 install udf /bin/false
 blacklist udf
 EOT
+modprobe -r udf
 
 # Disable usb-storage
 /bin/cat << EOT >/etc/modprobe.d/usb-storage.conf
 install usb-storage /bin/false
 blacklist usb-storage
 EOT
+modprobe -r usb-storage
 
 # File system configuration
 /bin/sed -ri 's/^\s*([^#]+\s+\/tmp\s+)(\S+\s+)(\S+)?(\s+[0-9]\s+[0-9].*)$/\1\2\3,noexec\4/' /etc/fstab
