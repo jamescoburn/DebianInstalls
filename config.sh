a64aea61d308da09b153e79ff7e17e805fbf3864
James Coburn <james.coburn@live.com>
2022-11-29 12:39:47 -0700

    Added Disable usb-storage section

--- config.sh
+++ config.sh
@@ -12,6 +12,12 @@ install udf /bin/false
 blacklist udf
 EOT
 
+# Disable usb-storage
+/bin/cat << EOT >/etc/modprobe.d/usb-storage.conf
+install usb-storage /bin/false
+blacklist usb-storage
+EOT
+
 # File system configuration
 /bin/sed -ri 's/^\s*([^#]+\s+\/tmp\s+)(\S+\s+)(\S+)?(\s+[0-9]\s+[0-9].*)$/\1\2\3,noexec\4/' /etc/fstab
 /bin/echo -e "tmpfs\t\t/dev/shm\ttmpfs\tnodev,nosuid,noexec\t0\t0" >> /etc/fstab
