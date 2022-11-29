272f41583a6c344942aae14fdba88cd48d6dc143
James Coburn <james.coburn@live.com>
2022-11-29 12:38:27 -0700

    Added Disable udf section

--- config.sh
+++ config.sh
@@ -2,11 +2,17 @@
 {
 # Disable squashfs
 /bin/cat << EOT >/etc/modprobe.d/squashfs.conf
-    install squashfs /bin/false
-    blacklist squashfs
+install squashfs /bin/false
+blacklist squashfs
 EOT
     
-    # File system configuration
-    /bin/sed -ri 's/^\s*([^#]+\s+\/tmp\s+)(\S+\s+)(\S+)?(\s+[0-9]\s+[0-9].*)$/\1\2\3,noexec\4/' /etc/fstab
-    /bin/echo -e "tmpfs\t\t/dev/shm\ttmpfs\tnodev,nosuid,noexec\t0\t0" >> /etc/fstab
+# Disable udf
+/bin/cat << EOT >/etc/modprobe.d/udf.conf
+install udf /bin/false
+blacklist udf
+EOT
+
+# File system configuration
+/bin/sed -ri 's/^\s*([^#]+\s+\/tmp\s+)(\S+\s+)(\S+)?(\s+[0-9]\s+[0-9].*)$/\1\2\3,noexec\4/' /etc/fstab
+/bin/echo -e "tmpfs\t\t/dev/shm\ttmpfs\tnodev,nosuid,noexec\t0\t0" >> /etc/fstab
 }
