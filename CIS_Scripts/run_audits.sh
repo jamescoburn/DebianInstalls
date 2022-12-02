36cef75033fd8dfe19b0e922aaaac2170175e6d0
James Coburn <james.coburn@live.com>
2022-12-02 12:31:48 -0700

    Added run_audits.sh

---
+++ CIS_Scripts/run_audits.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+sudo ./audit_cramfs.sh
+sudo ./audit_squashfs.sh
+sudo ./audit_udf.sh
+sudo ./audit_usbstorage.sh
+sudo ./audit_aslr.sh
+sudo ./audit_timesyncdaemons.sh
+sudo ./audit_ipv6.sh
+sudo ./audit_dccp.sh
+sudo ./audit_sctp.sh
+sudo ./audit_rds.sh
+sudo ./audit_tipc.sh
+sudo ./audit_sendredirects.sh
+sudo ./audit_ipforwarding.sh
+
