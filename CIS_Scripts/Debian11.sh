8025dda341a67f9d3b86a3ced83d87e741fed116
James Coburn <james.coburn@live.com>
2022-12-06 08:27:13 -0700

    Started building primary script; created variables; load functions

---
+++ CIS_Scripts/Debian11.sh
@@ -0,0 +1,66 @@
+if [ ! "$BASH_VERSION" ] ; then
+	exec /bin/bash "$0" "$@"
+fi
+
+# Set global variables
+BDIR="$(dirname "$(readlink -f "$0")")"
+FDIR=$BDIR/functions
+RECDIR="$FDIR"/recommendations
+GDIR="$FDIR"/general
+LDIR=$BDIR/logs
+RDIR=$BDIR/backup
+DTG=$(date +%m_%d_%Y_%H%M)
+mkdir $LDIR/$DTG
+mkdir $RDIR/$DTG
+LOGDIR=$LDIR/$DTG
+BKDIR=$RDIR/$DTG
+LOG=$LOGDIR/CIS-LBK_verbose.log
+SLOG=$LOGDIR/CIS-LBK.log
+ELOG=$LOGDIR/CIS-LBK_error.log
+FRLOG=$LOGDIR/CIS-LBK_failed.log
+MANLOG=$LOGDIR/CIS-LBK_manual.log
+passed_recommendations="0"
+failed_recommendations="0"
+remediated_recommendations="0"
+not_applicable_recommendations="0"
+excluded_recommendations="0"
+manual_recommendations="0"
+skipped_recommendations="0"
+total_recommendations="0"
+
+# Load functions (Order matters)
+for func in "$GDIR"/*.sh; do
+	[ -e "$func" ] || break
+	. "$func"
+done
+for func in "$RECDIR"/*.sh; do
+	[ -e "$func" ] || break
+	. "$func"
+done
+
+# Clear the screen for output
+clear
+
+# Display the build kit banner
+BANR
+
+# Ensure script is being run as root
+ROOTUSRCK
+
+# Display the terms of use
+# terms_of_use
+
+# Display CIS Linux Build Kit warning banner
+WARBNR
+
+#run_profile=L2S # Uncomment this line to provide profile to be run manually
+# Profile Options:
+# L1S - For Level 1 Server
+# L1W - For Level 1 Workstation
+# L2S - For Level 2 Server
+# L2W - For Level 2 Workstation
+# Have user select profile to run
+SELECT_PROFILE
+# Recommediations This is where a BM specific script begins.
+
+# Generated for specific Benchmark
