5e403edf50952e38e34f8d9e8a0423f98d69761f
James Coburn <james.coburn@live.com>
2022-12-06 08:49:07 -0700

    Added warning banner section to banners.sh

--- CIS_Scripts/functions/general/banners.sh
+++ CIS_Scripts/functions/general/banners.sh
@@ -13,3 +13,30 @@ BANR()
 	echo ""
 }
 # End of print bannor to Screen
+
+WARBNR()
+{
+	echo ""
+	echo ""
+	echo "  *************************************************************** "
+	echo "  *******WARNING*******WARNING*******WARNING*******WARNING******* "
+	echo "  *************************************************************** "
+	echo "  * *                                                         * * "
+	echo "  * *    This Linux Build Kit makes changes to your system    * * "
+	echo "  *W*    These changes could cause loss of functionality!     *W* "
+	echo "  *A*                                                         *A* "
+	echo "  *R*    Please ensure that this Linux Build Kit is tested    *R* "
+	echo "  *N*    in your testing environment before running it on a   *N* "
+	echo "  *I*    production system.                                   *I* "
+	echo "  *N*                                                         *N* "
+	echo "  *G*    Failure to employ proper testing may lead to         *G* "
+	echo "  * *    service interruption!                                * * "
+	echo "  * *                                                         * * "
+	echo "  *************************************************************** "
+	echo "  *******WARNING*******WARNING*******WARNING*******WARNING******* "
+	echo "  *************************************************************** "
+	echo ""
+	echo ""
+	CONFIRM
+}
+# End of Warning Banner
