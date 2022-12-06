43bead90c0d397ea52f073370ae07c0c3f85da22
James Coburn <james.coburn@live.com>
2022-12-06 08:30:07 -0700

    Added buildkit banner

---
+++ CIS_Scripts/functions/general/debian11_banner.sh
@@ -0,0 +1,15 @@
+BANR()
+{
+	echo ""
+	echo ""
+	echo "     ######################################################## "
+	echo "     #  CIS Linux Build Kit for Debian Linux 11             # "
+	echo "     #  This Linux Build Kit works in conjunction with the  # "
+	echo "     #  CIS Debian Linux 11 Benchmark v1.0.0                # "
+#	echo "     #  This script is the property of:                     # "
+#	echo "     #  Center for Internet Security (CIS)                  # "
+	echo "     ######################################################## "
+	echo ""
+	echo ""
+}
+# End of print bannor to Screen
