8cd7374f736087ea4c217a83b83277eae2b7bd54
James Coburn <james.coburn@live.com>
2022-12-06 10:20:13 -0700

    Added function content to module_loadable_fix function

--- CIS_Scripts/functions/general/modules.sh
+++ CIS_Scripts/functions/general/modules.sh
@@ -1,6 +1,23 @@
 function module_loadable_fix()
 {
-    
+output=""
+output=$(modprobe -n -v "$XCCDF_VALUE_REGEX")
+
+if modprobe -n -v "$XCCDF_VALUE_REGEX" | grep -Eq "^\s*install\s+\/bin\/(true|false)\b"; then
+	output=$(modprobe -n -v "$XCCDF_VALUE_REGEX")
+#	echo "PASSED, \"$output\""
+	return "${XCCDF_RESULT_PASS:-101}"
+else
+	echo "install $XCCDF_VALUE_REGEX /bin/true" >> /etc/modprobe.d/"$XCCDF_VALUE_REGEX".conf
+	output=$(modprobe -n -v "$XCCDF_VALUE_REGEX")
+	if modprobe -n -v "$XCCDF_VALUE_REGEX" | grep -Eq "^\s*install\s+\/bin\/(true|false)\b"; then
+#		echo "PASSED, \"$output\""
+		return "${XCCDF_RESULT_PASS:-103}"
+	else
+#		echo "Failed: \"$output\""
+    	return "${XCCDF_RESULT_FAIL:-102}"
+    fi
+fi
 }
 
 function module_loaded_fix()
