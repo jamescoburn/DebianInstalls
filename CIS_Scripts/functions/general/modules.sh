cc668b22e4e11545297da77acdc3adf000ba606b
James Coburn <james.coburn@live.com>
2022-12-06 10:21:27 -0700

    Added function content to module_loaded_fix function

--- CIS_Scripts/functions/general/modules.sh
+++ CIS_Scripts/functions/general/modules.sh
@@ -22,5 +22,22 @@ fi
 
 function module_loaded_fix()
 {
+output=""
+output=$(lsmod | grep "$XCCDF_VALUE_REGEX")
 
+if [ -z "$(lsmod | grep "$XCCDF_VALUE_REGEX")" ]; then
+	output=$(modprobe -n -v "$XCCDF_VALUE_REGEX")
+#	echo "PASSED, \"$output\""
+	return "${XCCDF_RESULT_PASS:-101}"
+else
+	rmmod "$XCCDF_VALUE_REGEX"
+	output=$(lsmod | grep "$XCCDF_VALUE_REGEX")
+	if [ -z "$(lsmod | grep "$XCCDF_VALUE_REGEX")" ]; then
+#		echo "PASSED, \"$output\""
+		return "${XCCDF_RESULT_PASS:-103}"
+	else
+#		echo "Failed: \"$output\""
+    	return "${XCCDF_RESULT_FAIL:-102}"
+    fi
+fi
 }
