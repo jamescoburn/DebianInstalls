e46202ff09415f03d3f8befbca38829b3703b674
James Coburn <james.coburn@live.com>
2023-07-29 12:17:23 -0600

    Modified for MongoDB 3.6.23

--- unifiquickdirty.sh
+++ unifiquickdirty.sh
@@ -1,10 +1,10 @@
 # Install MongoDB Debian Dependancies
 sudo apt-get install gnupg2 wget -y
-echo "deb http://repo.mongodb.org/apt/debian buster/mongodb-org/5.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
+echo "deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/3.6 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
 sudo apt-get update -y
 
 # Integrate GPG key for MongoDB
-curl -sSL https://www.mongodb.org/static/pgp/server-5.0.asc  -o mongoserver.asc
+curl -sSL https://www.mongodb.org/static/pgp/server-3.6.asc  -o mongoserver.asc
 gpg --no-default-keyring --keyring ./mongo_key_temp.gpg --import ./mongoserver.asc
 gpg --no-default-keyring --keyring ./mongo_key_temp.gpg --export > ./mongoserver_key.gpg
 sudo mv mongoserver_key.gpg /etc/apt/trusted.gpg.d/
@@ -13,7 +13,7 @@ sudo mv mongoserver_key.gpg /etc/apt/trusted.gpg.d/
 sudo apt update
 
 # Install MongoDB on Debian11 Bullseye
-sudo apt install mongodb-org
+sudo apt install mongodb-org=3.6.23 mongodb-org-server=3.6.23 mongodb-org-shell=3.6.23 mongodb-org-mongos=3.6.23 mongodb-org-tools=3.6.23
 
 # Enable and start MongoDB service
 sudo systemctl enable --now mongod
