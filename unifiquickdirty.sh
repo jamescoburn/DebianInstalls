c96d36063f7778161cde242b855d13d5f1fd5031
James Coburn <james.coburn@live.com>
2023-07-29 13:04:56 -0600

    Added Unifi Network Application section

--- unifiquickdirty.sh
+++ unifiquickdirty.sh
@@ -2,7 +2,6 @@
 # Install MongoDB Debian Dependancies
 sudo wget -O /etc/apt/trusted.gpg.d/mongoserver_key.gpg https://www.mongodb.org/static/pgp/server-3.6.asc
 echo "deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/3.6 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
-sudo apt-get update -y
 
 # Run system update
 sudo apt update
@@ -14,3 +13,13 @@ sudo apt install mongodb-org=3.6.23 mongodb-org-server=3.6.23 mongodb-org-shell=
 sudo systemctl enable --now mongod
 sudo systemctl status mongod
 
+########## UNIFI ##########
+# Unifi repository gpg key
+sudo wget -O /etc/apt/trusted.gpg.d/unifi-repo.gpg https://dl.ui.com/unifi/unifi-repo.gpg
+echo 'deb https://www.ui.com/downloads/unifi/debian stable ubiquiti' | sudo tee /etc/apt/sources.list.d/100-ubnt-unifi.list
+
+# Run system update
+sudo apt update
+
+# Install Unifi Network Application
+sudo apt install unifi -y
